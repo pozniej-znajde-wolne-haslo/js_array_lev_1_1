9972879cfe9d9c341ab83a7c74a486f09e978a2f
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-01-31 12:15:33 +0100

    adds ready exercise in js (trying out array values & their index)

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const numberArray = [5, 6, 7, 8, 9, 10];
+console.log(numberArray);
+
+console.log(numberArray[4]);
+console.log(numberArray[0]);
+console.log(numberArray[1]);
+console.log(numberArray[2]);
+console.log(numberArray[3]);
+console.log(numberArray[5]);
+console.log(numberArray.length);
+
+/* Erstelle ein Array numberArray, das Zahlen von 5 bis 10 enthält.
+Gib es in der Konsole aus.
+Die blau hinterlegten Ziffern geben den Index an.
+
+Arrays sind 0-indiziert, was bedeutet, dass sie bei 0 und nicht bei 1 zu zählen beginnen.
+Wenn wir von numberArray die Nummer 9 aufrufen wollen, müssen wir numberArray[4]; aufrufen.
+Gib es in der Konsole aus.
+Rufe auch die Zahlen 5 und 10 auf.
+
+
+Um das Thema Array Index besser zu verstehen, teste die Visualisierungsfunktion dieser Seite:
+https://codepen.io/patik/full/vzBwrO
+
+Lese Analogien über den Arrays-Index, der hier am Beispiel des Buches Harry Potter erklärt wird.
+https://blog.codeanalogies.com/2017/04/29/javascript-arrays-and-objects-are-just-like-books-and-newspapers/
+
+ */
